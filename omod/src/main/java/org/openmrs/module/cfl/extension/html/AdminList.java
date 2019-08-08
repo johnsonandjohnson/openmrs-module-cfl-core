cf60aebe559d5938d5f7096c091d4bff8253101e
Arkadiusz Lalo <alalo@soldevelo.com>
2019-08-08 10:09:29 +0200

    CFLM-26: Cleaned up the module structure

--- omod/src/main/java/org/openmrs/module/cfl/extension/html/AdminList.java
+++ omod/src/main/java/org/openmrs/module/cfl/extension/html/AdminList.java
@@ -1,12 +1,3 @@
-/**
- * This Source Code Form is subject to the terms of the Mozilla Public License,
- * v. 2.0. If a copy of the MPL was not distributed with this file, You can
- * obtain one at http://mozilla.org/MPL/2.0/. OpenMRS is also distributed under
- * the terms of the Healthcare Disclaimer located at http://openmrs.org/license.
- *
- * Copyright (C) OpenMRS Inc. OpenMRS is a registered trademark and the OpenMRS
- * graphic logo is a trademark of OpenMRS Inc.
- */
 package org.openmrs.module.cfl.extension.html;
 
 import java.util.HashMap;
@@ -16,8 +7,8 @@ import org.openmrs.module.Extension;
 import org.openmrs.module.web.extension.AdministrationSectionExt;
 
 /**
- * This class defines the links that will appear on the administration page under the
- * "cfl.title" heading. This extension is enabled by defining (uncommenting) it in the config.xml file.
+ * This class defines the links that will appear on the administration page under the "cfl.title"
+ * heading. This extension is enabled by defining (uncommenting) it in the config.xml file.
  */
 public class AdminList extends AdministrationSectionExt {
 	
@@ -44,7 +35,7 @@ public class AdminList extends AdministrationSectionExt {
 		
 		map.put("module/cfl/cfl.form", "cfl.title");
 		map.put("cfl/cfl.page", "cfl.refapp.title");
-
+		
 		return map;
 	}
 	
