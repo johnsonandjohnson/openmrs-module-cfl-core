3d1c78df5fb2218688d1d48acf9f0658ec1a4daa
Arkadiusz Lalo <alalo@soldevelo.com>
2019-08-12 10:27:49 +0200

    CFLM-27: Added the CFL HIV form

---
+++ api/src/main/java/org/openmrs/module/cfl/CFLConstants.java
@@ -0,0 +1,9 @@
+package org.openmrs.module.cfl;
+
+public final class CFLConstants {
+	
+	public static final String MODULE_ID = "cfl";
+	
+	private CFLConstants() {
+	};
+}
--- api/src/main/java/org/openmrs/module/cfl/CFLModuleActivator.java
+++ api/src/main/java/org/openmrs/module/cfl/CFLModuleActivator.java
@@ -2,7 +2,17 @@ package org.openmrs.module.cfl;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import org.openmrs.api.FormService;
+import org.openmrs.api.context.Context;
 import org.openmrs.module.BaseModuleActivator;
+import org.openmrs.module.Module;
+import org.openmrs.module.ModuleFactory;
+import org.openmrs.module.htmlformentry.HtmlFormEntryService;
+import org.openmrs.module.htmlformentryui.HtmlFormUtil;
+import org.openmrs.ui.framework.resource.ResourceFactory;
+
+import java.util.Arrays;
+import java.util.List;
 
 /**
  * This class contains the logic that is run every time this module is either started or shutdown
@@ -16,6 +26,14 @@ public class CFLModuleActivator extends BaseModuleActivator {
 	 */
 	public void started() {
 		log.info("Started CFL Module");
+		try {
+			setupHtmlForms();
+		}
+		catch (Exception e) {
+			Module mod = ModuleFactory.getModuleById(CFLConstants.MODULE_ID);
+			ModuleFactory.stopModule(mod);
+			throw new RuntimeException("failed to setup the required modules", e);
+		}
 	}
 	
 	/**
@@ -25,4 +43,16 @@ public class CFLModuleActivator extends BaseModuleActivator {
 		log.info("Shutdown CFL Module");
 	}
 	
+	private void setupHtmlForms() throws Exception {
+		ResourceFactory resourceFactory = ResourceFactory.getInstance();
+		FormService formService = Context.getFormService();
+		HtmlFormEntryService htmlFormEntryService = Context.getService(HtmlFormEntryService.class);
+
+		List<String> htmlforms = Arrays.asList("cfl:htmlforms/cfl-HIV.xml");
+
+		for (String htmlform : htmlforms) {
+			HtmlFormUtil.getHtmlFormFromUiResource(resourceFactory, formService, htmlFormEntryService, htmlform);
+		}
+	}
+
 }
