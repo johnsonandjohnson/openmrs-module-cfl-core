b6a992e2b00ce364deb5182271af2f5073067eb7
Wojciech Buława <wbulawa@soldevelo.com>
2020-02-19 11:11:03 +0000

    CFLM-196: Added breadcrumbs at find caregiver page
    Approved-by: Daniel Serkowski <dserkowski@soldevelo.com>

--- omod/src/main/java/org/openmrs/module/cfl/page/controller/FindPersonPageController.java
+++ omod/src/main/java/org/openmrs/module/cfl/page/controller/FindPersonPageController.java
@@ -1,28 +1,30 @@
 package org.openmrs.module.cfl.page.controller;
 
 import org.openmrs.module.appframework.domain.AppDescriptor;
-import org.openmrs.module.appui.UiSessionContext;
-import org.openmrs.module.coreapps.helper.BreadcrumbHelper;
-import org.openmrs.ui.framework.UiUtils;
 import org.openmrs.ui.framework.page.PageModel;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import static org.openmrs.module.cfl.CFLRegisterPersonConstants.APP_PROP;
+import static org.openmrs.module.cfl.CFLRegisterPersonConstants.BREADCRUMB_OVERRIDE_PROP;
 
 /**
  * It is based of org.openmrs.module.coreapps.page.controller.findpatient.FindPatientPageController
  */
 public class FindPersonPageController {
     /**
-     * This page is built to be shared across multiple apps. To use it, you must pass an "app"
+     * This page is built to be shared across multiple apps. To use it, you must pass an APP_PROP
      * request parameter, which must be the id of an existing app that is an instance of
      * coreapps.template.findPatient
      *
      * @param model
      * @param app
-     * @param sessionContext
+     * @param breadcrumbOverride
      */
-    public void get(PageModel model, @RequestParam("app") AppDescriptor app, UiSessionContext sessionContext,
-                    UiUtils ui) {
+    public void get(PageModel model,
+                    @RequestParam(APP_PROP) AppDescriptor app,
+                    @RequestParam(value = BREADCRUMB_OVERRIDE_PROP, required = false) String breadcrumbOverride) {
 
+        model.addAttribute(BREADCRUMB_OVERRIDE_PROP, breadcrumbOverride);
         model.addAttribute("afterSelectedUrl", app.getConfig().get("afterSelectedUrl").getTextValue());
         model.addAttribute("heading", app.getConfig().get("heading").getTextValue());
         model.addAttribute("label", app.getConfig().get("label").getTextValue());
@@ -32,6 +34,5 @@ public class FindPersonPageController {
         } else {
             model.addAttribute("registrationAppLink", app.getConfig().get("registrationAppLink").getTextValue());
         }
-        BreadcrumbHelper.addBreadcrumbsIfDefinedInApp(app, model, ui);
     }
 }
