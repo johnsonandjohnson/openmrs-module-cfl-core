6232bba0f30fc97c30a1e8e1eedb804b2357b0a3
Piotr Wargulak <pwargulak@soldevelo.com>
2021-03-12 14:45:12 +0000

    AGRE-397: Fixed initial Concept configuration
    * AGRE-397: Fixed initial Concept configuration
    
    Solved:
    - Concepts added by CfL module (HIV Risk Factor form) ware missing Fully Specified Names and ware depending on Concepts added later during startup process
    - The covid related package from reference metadata module have duplicate one Concept Term which needs to be removed by us
    
    * AGRE-397: Code review changes
    
    * AGRE-397: Code review changes pt2
    
    Approved-by: Dawid Ruchniewicz

--- api/src/main/java/org/openmrs/module/cfl/api/util/DataCleanup.java
+++ api/src/main/java/org/openmrs/module/cfl/api/util/DataCleanup.java
@@ -2,12 +2,16 @@ package org.openmrs.module.cfl.api.util;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import org.openmrs.Concept;
+import org.openmrs.ConceptMap;
+import org.openmrs.ConceptReferenceTerm;
 import org.openmrs.Location;
 import org.openmrs.PatientIdentifierType;
 import org.openmrs.PersonAttributeType;
 import org.openmrs.Provider;
 import org.openmrs.RelationshipType;
 import org.openmrs.User;
+import org.openmrs.api.ConceptService;
 import org.openmrs.api.LocationService;
 import org.openmrs.api.PatientService;
 import org.openmrs.api.PersonService;
@@ -23,9 +27,12 @@ public final class DataCleanup {
 
     private static final Log LOGGER = LogFactory.getLog(DataCleanup.class);
 
+    private DataCleanup() {
+    }
+
     public static void cleanUpUnnecessaryData() {
-        if (!Boolean.parseBoolean(Context.getAdministrationService()
-                .getGlobalProperty(CFLConstants.CFL_DISTRO_BOOTSTRAPPED_KEY))) {
+        if (!Boolean.parseBoolean(
+                Context.getAdministrationService().getGlobalProperty(CFLConstants.CFL_DISTRO_BOOTSTRAPPED_KEY))) {
             removeUnnecessaryData();
             createAndUpdateNecessaryData();
             markInitialDataCleanupAsDone();
@@ -41,6 +48,7 @@ public final class DataCleanup {
         removeUnnecessaryPersonAttributeTypes();
         removeUnnecessaryProviders();
         removeUnnecessaryLocations();
+        fixInvalidConceptReferenceTerm();
     }
 
     private static void createAndUpdateNecessaryData() {
@@ -52,14 +60,13 @@ public final class DataCleanup {
     }
 
     private static void markInitialDataCleanupAsDone() {
-        Context.getAdministrationService().setGlobalProperty(
-                CFLConstants.CFL_DISTRO_BOOTSTRAPPED_KEY, CFLConstants.TRUE);
+        Context.getAdministrationService().setGlobalProperty(CFLConstants.CFL_DISTRO_BOOTSTRAPPED_KEY, CFLConstants.TRUE);
     }
 
     private static void removeUnnecessaryUsers() {
         UserService userService = Context.getUserService();
-        List<String> usersToRemove = getSplitPropertiesBySeparator(
-                CFLConstants.USERS_TO_REMOVE, CFLConstants.COMMA_SEPARATOR);
+        List<String> usersToRemove =
+                getSplitPropertiesBySeparator(CFLConstants.USERS_TO_REMOVE, CFLConstants.COMMA_SEPARATOR);
         for (User user : userService.getAllUsers()) {
             if (usersToRemove.contains(user.getUsername())) {
                 userService.purgeUser(user);
@@ -69,8 +76,8 @@ public final class DataCleanup {
 
     private static void removeUnnecessaryPatientIdentifierTypes() {
         PatientService patientService = Context.getPatientService();
-        List<String> patientIdentifierTypesToRemove = getSplitPropertiesBySeparator(
-                CFLConstants.PATIENT_IDENTIFIER_TYPES_TO_REMOVE, CFLConstants.COMMA_SEPARATOR);
+        List<String> patientIdentifierTypesToRemove =
+                getSplitPropertiesBySeparator(CFLConstants.PATIENT_IDENTIFIER_TYPES_TO_REMOVE, CFLConstants.COMMA_SEPARATOR);
         for (PatientIdentifierType patientIdentifierType : patientService.getAllPatientIdentifierTypes()) {
             if (patientIdentifierTypesToRemove.contains(patientIdentifierType.getName())) {
                 patientService.purgePatientIdentifierType(patientIdentifierType);
@@ -80,8 +87,8 @@ public final class DataCleanup {
 
     private static void removeUnnecessaryRelationshipTypes() {
         PersonService personService = Context.getPersonService();
-        List<String> relationshipTypesUuidsToRemove = getSplitPropertiesBySeparator(
-                CFLConstants.RELATIONSHIP_TYPES_UUIDS_TO_REMOVE, CFLConstants.COMMA_SEPARATOR);
+        List<String> relationshipTypesUuidsToRemove =
+                getSplitPropertiesBySeparator(CFLConstants.RELATIONSHIP_TYPES_UUIDS_TO_REMOVE, CFLConstants.COMMA_SEPARATOR);
         for (RelationshipType relationshipType : personService.getAllRelationshipTypes(true)) {
             if (relationshipTypesUuidsToRemove.contains(relationshipType.getUuid())) {
                 personService.purgeRelationshipType(relationshipType);
@@ -91,8 +98,8 @@ public final class DataCleanup {
 
     private static void removeUnnecessaryPersonAttributeTypes() {
         PersonService personService = Context.getPersonService();
-        List<String> personAttributeTypesToRemove = getSplitPropertiesBySeparator(
-                CFLConstants.PERSON_ATTRIBUTE_TYPES_TO_REMOVE, CFLConstants.COMMA_SEPARATOR);
+        List<String> personAttributeTypesToRemove =
+                getSplitPropertiesBySeparator(CFLConstants.PERSON_ATTRIBUTE_TYPES_TO_REMOVE, CFLConstants.COMMA_SEPARATOR);
         for (PersonAttributeType personAttributeType : personService.getAllPersonAttributeTypes()) {
             if (personAttributeTypesToRemove.contains(personAttributeType.getName())) {
                 personService.purgePersonAttributeType(personAttributeType);
@@ -102,8 +109,8 @@ public final class DataCleanup {
 
     private static void removeUnnecessaryProviders() {
         ProviderService providerService = Context.getProviderService();
-        List<String> providersToRemove = getSplitPropertiesBySeparator(
-                CFLConstants.PROVIDERS_TO_REMOVE, CFLConstants.COMMA_SEPARATOR);
+        List<String> providersToRemove =
+                getSplitPropertiesBySeparator(CFLConstants.PROVIDERS_TO_REMOVE, CFLConstants.COMMA_SEPARATOR);
         for (Provider provider : providerService.getAllProviders()) {
             if (providersToRemove.contains(provider.getIdentifier())) {
                 providerService.purgeProvider(provider);
@@ -113,8 +120,8 @@ public final class DataCleanup {
 
     private static void removeUnnecessaryLocations() {
         LocationService locationService = Context.getLocationService();
-        List<String> locationsUuidsToRemove = getSplitPropertiesBySeparator(
-                CFLConstants.LOCATION_UUIDS_TO_REMOVE, CFLConstants.COMMA_SEPARATOR);
+        List<String> locationsUuidsToRemove =
+                getSplitPropertiesBySeparator(CFLConstants.LOCATION_UUIDS_TO_REMOVE, CFLConstants.COMMA_SEPARATOR);
         for (Location location : locationService.getAllLocations()) {
             if (locationsUuidsToRemove.contains(location.getUuid()) && location.getParentLocation() == null) {
                 locationService.purgeLocation(location);
@@ -157,6 +164,42 @@ public final class DataCleanup {
         return Arrays.asList(property.split(separator));
     }
 
-    private DataCleanup() {
+    /**
+     * Fix the invalid ConceptReferenceTerm added by openmrs-module-referencemetadata/Covid-19_Concepts-1.xml
+     */
+    private static void fixInvalidConceptReferenceTerm() {
+        final String validConfirmedConceptTermUuid = "a69dafbf-5b89-3c59-a7e2-8c0477ca4bc0";
+        final String invalidConfirmedConceptTermUuid = "4ebd268b-a4fe-369d-82ff-353a0a922c20";
+        final String invalidConceptFromCovidRefMetadataUuid = "165792AAAAAAAAAAAAAAAAAAAAAAAAAAAAAA";
+
+        final ConceptService conceptService = Context.getConceptService();
+
+        final ConceptReferenceTerm invalidTerm =
+                conceptService.getConceptReferenceTermByUuid(invalidConfirmedConceptTermUuid);
+        final Concept conceptWithInvalidTerm = conceptService.getConceptByUuid(invalidConceptFromCovidRefMetadataUuid);
+        final ConceptReferenceTerm validTerm = conceptService.getConceptReferenceTermByUuid(validConfirmedConceptTermUuid);
+
+        if (invalidTerm == null || conceptWithInvalidTerm == null) {
+            LOGGER.info(new StringBuilder("Invalid Concept data [term: ")
+                    .append(invalidConfirmedConceptTermUuid)
+                    .append(", concept:")
+                    .append(invalidConceptFromCovidRefMetadataUuid)
+                    .append(")] from openmrs-module-referencemetadata/Covid-19_Concepts-1.xml ware not found.")
+                    .toString());
+        } else if (validTerm == null) {
+            LOGGER.error(new StringBuilder("Concept [")
+                    .append(validConfirmedConceptTermUuid)
+                    .append("] used to fix openmrs-module-referencemetadata/Covid-19_Concepts-1.xml was NOT found! ")
+                    .append("The system may be unstable!")
+                    .toString());
+        } else {
+            for (final ConceptMap conceptMap : conceptWithInvalidTerm.getConceptMappings()) {
+                if (conceptMap.getConceptReferenceTerm().equals(invalidTerm)) {
+                    conceptMap.setConceptReferenceTerm(validTerm);
+                }
+            }
+
+            conceptService.purgeConceptReferenceTerm(invalidTerm);
+        }
     }
 }
