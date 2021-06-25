3fc54f10cfc5672c79013589a0651bff0dedc265
Piotr Wargulak <pwargulak@soldevelo.com>
2021-06-25 10:08:19 +0200

    AGRE-1571: Initial commit - added empty project

---
+++ api/src/main/java/org/openmrs/module/cfldistribution/CfldistributionModuleActivator.java
@@ -0,0 +1,34 @@
+/**
+ * The contents of this file are subject to the OpenMRS Public License
+ * Version 1.0 (the "License"); you may not use this file except in
+ * compliance with the License. You may obtain a copy of the License at
+ * http://license.openmrs.org
+ * <p>
+ * Software distributed under the License is distributed on an "AS IS"
+ * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See the
+ * License for the specific language governing rights and limitations
+ * under the License.
+ * <p>
+ * Copyright (C) OpenMRS, LLC.  All Rights Reserved.
+ */
+package org.openmrs.module.cfldistribution;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.openmrs.module.BaseModuleActivator;
+
+/**
+ * This class contains the logic that is run every time this module is either started or shutdown
+ */
+public class CfldistributionModuleActivator extends BaseModuleActivator {
+
+    private Log log = LogFactory.getLog(this.getClass());
+
+    public void startup() {
+        log.info("Starting Cfl Distribution Module");
+    }
+
+    public void shutdown() {
+        log.info("Shutting down Cfl Distribution Module");
+    }
+}
