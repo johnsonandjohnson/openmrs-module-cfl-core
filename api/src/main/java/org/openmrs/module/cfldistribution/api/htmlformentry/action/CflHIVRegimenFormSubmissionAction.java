8ccc1a0db6723c0e0d6023d3d177e139cf1910a4
druchniewicz <druchniewicz@soldevelo.com>
2021-11-25 16:09:27 +0100

    AGRE-1922: Adjusted hiv form with new custom regimen tag

---
+++ api/src/main/java/org/openmrs/module/cfldistribution/api/htmlformentry/action/CflHIVRegimenFormSubmissionAction.java
@@ -0,0 +1,12 @@
+package org.openmrs.module.cfldistribution.api.htmlformentry.action;
+
+import org.openmrs.module.htmlformentry.CustomFormSubmissionAction;
+import org.openmrs.module.htmlformentry.FormEntrySession;
+
+public class CflHIVRegimenFormSubmissionAction implements CustomFormSubmissionAction {
+
+  @Override
+  public void applyAction(FormEntrySession formEntrySession) {
+    //so far do nothing
+  }
+}
--- api/src/main/java/org/openmrs/module/cfldistribution/api/metadata/HtmlFormsMetadata.java
+++ api/src/main/java/org/openmrs/module/cfldistribution/api/metadata/HtmlFormsMetadata.java
@@ -10,36 +10,41 @@ import org.openmrs.ui.framework.resource.ResourceFactory;
 import java.util.Arrays;
 import java.util.List;
 
-/**
- * Loads all HTML forms from `omod/src/main/resources/htmlforms`.
- */
+/** Loads all HTML forms from `omod/src/main/resources/htmlforms`. */
 public class HtmlFormsMetadata extends VersionedMetadataBundle {
 
-    @Override
-    public int getVersion() {
-        return 1;
+  @Override
+  public int getVersion() {
+    return 1;
+  }
+
+  @Override
+  protected void installEveryTime() {
+    // nothing to do
+  }
+
+  @Override
+  protected void installNewVersion() throws Exception {
+    final ResourceFactory resourceFactory = ResourceFactory.getInstance();
+    final FormService formService = Context.getFormService();
+    final HtmlFormEntryService htmlFormEntryService =
+        Context.getService(HtmlFormEntryService.class);
+
+    final List<String> htmlforms =
+        Arrays.asList(
+            "cfldistribution:htmlforms/cfl-HIV.xml",
+            "cfldistribution:htmlforms/cfl-check-in.xml",
+            "cfldistribution:htmlforms/cfl-visit-note.xml",
+            "cfldistribution:htmlforms/cfl-medicine-refill.xml",
+            "cfldistribution:htmlforms/cfl-sputum-visit-note.xml",
+            "cfldistribution:htmlforms/encounters-form.xml",
+            "cfldistribution:htmlforms/cfl-hiv-enrollment.xml",
+            "cfldistribution:htmlforms/discontinue-program.xml",
+            "cfldistribution:htmlforms/cfl-hiv-arts.xml");
+
+    for (String htmlform : htmlforms) {
+      HtmlFormUtil.getHtmlFormFromUiResource(
+          resourceFactory, formService, htmlFormEntryService, htmlform);
     }
-
-    @Override
-    protected void installEveryTime() {
-        // nothing to do
-    }
-
-    @Override
-    protected void installNewVersion() throws Exception {
-        final ResourceFactory resourceFactory = ResourceFactory.getInstance();
-        final FormService formService = Context.getFormService();
-        final HtmlFormEntryService htmlFormEntryService = Context.getService(HtmlFormEntryService.class);
-
-        final List<String> htmlforms =
-                Arrays.asList("cfldistribution:htmlforms/cfl-HIV.xml", "cfldistribution:htmlforms/cfl-check-in.xml",
-                        "cfldistribution:htmlforms/cfl-visit-note.xml", "cfldistribution:htmlforms/cfl-medicine-refill.xml",
-                        "cfldistribution:htmlforms/cfl-sputum-visit-note.xml",
-                        "cfldistribution:htmlforms/encounters-form.xml", "cfldistribution:htmlforms/cfl-hiv-enrollment.xml",
-                        "cfldistribution:htmlforms/discontinue-program.xml");
-
-        for (String htmlform : htmlforms) {
-            HtmlFormUtil.getHtmlFormFromUiResource(resourceFactory, formService, htmlFormEntryService, htmlform);
-        }
-    }
+  }
 }
