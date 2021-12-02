674ef26578b7febda4b34b59f1007adbef667556
druchniewicz <druchniewicz@soldevelo.com>
2021-12-02 16:55:00 +0100

    AGRE-1972: Refactored code to use openmrs app-related entities

--- api/src/main/java/org/openmrs/module/cfl/advice/SaveUserAppAdvice.java
+++ api/src/main/java/org/openmrs/module/cfl/advice/SaveUserAppAdvice.java
@@ -3,21 +3,28 @@ package org.openmrs.module.cfl.advice;
 import org.apache.commons.collections.CollectionUtils;
 import org.apache.commons.lang.StringUtils;
 import org.codehaus.jackson.map.ObjectMapper;
-import org.openmrs.api.context.Context;
+import org.codehaus.jackson.map.annotate.JsonSerialize;
+import org.openmrs.module.appframework.domain.AppDescriptor;
+import org.openmrs.module.appframework.domain.Extension;
 import org.openmrs.module.appframework.domain.UserApp;
-import org.openmrs.module.appframework.service.AppFrameworkService;
-import org.openmrs.module.cfl.api.model.UserAppContent;
-import org.openmrs.module.cfl.api.model.UserAppExtension;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.aop.AfterReturningAdvice;
+import org.springframework.aop.MethodBeforeAdvice;
 
 import java.io.IOException;
 import java.lang.reflect.Method;
 import java.util.List;
 import java.util.stream.Collectors;
 
-public class SaveUserAppAdvice implements AfterReturningAdvice {
+/**
+ * The SaveUserAppAdvice class.
+ *
+ * <p>The implemented method is invoked before calling saveUserApp method from AppFrameworkService.
+ * The main reason of this method is replacing apostrophe sign (') with XML-encoded representation
+ * in app extensions of script type. This is made because of different UI pages read differently
+ * these signs.
+ */
+public class SaveUserAppAdvice implements MethodBeforeAdvice {
 
   private static final Logger LOGGER = LoggerFactory.getLogger(SaveUserAppAdvice.class);
 
@@ -30,55 +37,46 @@ public class SaveUserAppAdvice implements AfterReturningAdvice {
   private static final String SCRIPT_TYPE_NAME = "script";
 
   @Override
-  public void afterReturning(Object o, Method method, Object[] objects, Object o1) {
+  public void before(Method method, Object[] objects, Object o) throws Throwable {
     if (method.getName().equals(SAVE_USER_APP_METHOD_NAME)) {
-      replaceApostropheCharsIfRequired(o);
+      replaceApostropheCharsIfRequired((UserApp) objects[0]);
     }
   }
 
-  private void replaceApostropheCharsIfRequired(Object o) {
-    if (o instanceof UserApp) {
-      UserApp app = (UserApp) o;
-      String appJson = app.getJson();
-      StringBuilder sb = new StringBuilder(appJson);
-      try {
-        ObjectMapper objectMapper = new ObjectMapper();
-        UserAppContent userAppContent = objectMapper.readValue(appJson, UserAppContent.class);
-        List<UserAppExtension> extensions = getExtensionsWithScriptType(userAppContent);
-        if (CollectionUtils.isNotEmpty(extensions)) {
-          replaceApostropheChars(sb, extensions);
-          setAndSaveUserApp(app, sb.toString());
-        }
-      } catch (IOException ex) {
-        LOGGER.error(
-            String.format(
-                "Cannot properly map JSON string into %s class", UserAppContent.class.getName()));
+  private void replaceApostropheCharsIfRequired(UserApp app) {
+    try {
+      AppDescriptor appDescriptor = getObjectMapper().readValue(app.getJson(), AppDescriptor.class);
+      List<Extension> extensions = getExtensionWithScriptType(appDescriptor);
+      if (CollectionUtils.isNotEmpty(extensions)) {
+        replaceApostropheChars(extensions);
+        app.setJson(
+            getObjectMapper().writerWithDefaultPrettyPrinter().writeValueAsString(appDescriptor));
       }
+    } catch (IOException ex) {
+      LOGGER.error(
+          String.format(
+              "Cannot properly map JSON string into %s class", AppDescriptor.class.getName()));
     }
   }
 
-  private List<UserAppExtension> getExtensionsWithScriptType(UserAppContent userAppContent) {
-    return userAppContent.getExtensions().stream()
-        .filter(
-            ext ->
-                StringUtils.isNotBlank(ext.getScript())
-                    && StringUtils.equalsIgnoreCase(ext.getType(), SCRIPT_TYPE_NAME)
-                    && ext.getScript().contains(APOSTROPHE_CHAR))
+  private ObjectMapper getObjectMapper() {
+    ObjectMapper objectMapper = new ObjectMapper();
+    objectMapper.setSerializationInclusion(JsonSerialize.Inclusion.NON_NULL);
+    return objectMapper;
+  }
+
+  private List<Extension> getExtensionWithScriptType(AppDescriptor appDescriptor) {
+    return appDescriptor.getExtensions().stream()
+        .filter(ext -> StringUtils.isNotBlank(ext.getScript()))
+        .filter(ext -> StringUtils.equalsIgnoreCase(ext.getType(), SCRIPT_TYPE_NAME))
+        .filter(ext -> ext.getScript().contains(APOSTROPHE_CHAR))
         .collect(Collectors.toList());
   }
 
-  private void replaceApostropheChars(StringBuilder sb, List<UserAppExtension> extensions) {
-    for (UserAppExtension extension : extensions) {
+  private void replaceApostropheChars(List<Extension> extensions) {
+    for (Extension extension : extensions) {
       String scriptFieldValue = extension.getScript();
-      int startIndex = sb.indexOf(scriptFieldValue);
-      int endIndex = startIndex + scriptFieldValue.length();
-      scriptFieldValue = scriptFieldValue.replace(APOSTROPHE_CHAR, APOSTROPHE_CHAR_UNICODE);
-      sb.replace(startIndex, endIndex, scriptFieldValue);
+      extension.setScript(scriptFieldValue.replace(APOSTROPHE_CHAR, APOSTROPHE_CHAR_UNICODE));
     }
-  }
-
-  private void setAndSaveUserApp(UserApp app, String appJson) {
-    app.setJson(appJson);
-    Context.getService(AppFrameworkService.class).saveUserApp(app);
   }
 }
