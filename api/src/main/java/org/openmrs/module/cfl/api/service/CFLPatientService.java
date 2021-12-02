e8511d57d8767152a640e78d3bb0e3237aa29985
Piotr Wargulak <pwargulak@soldevelo.com>
2021-12-02 12:10:01 +0100

    AGRE-1843: Fixed Wrong Count of records displayed on Find patient record screen
    - added location based access logic to patient count
    
    (cherry picked from commit 965949b4d3c85fa29e1f87b92437a1edaed873b4)

--- api/src/main/java/org/openmrs/module/cfl/api/service/CFLPatientService.java
+++ api/src/main/java/org/openmrs/module/cfl/api/service/CFLPatientService.java
@@ -1,10 +1,17 @@
 package org.openmrs.module.cfl.api.service;
 
 import org.openmrs.Patient;
+import org.openmrs.annotation.Authorized;
 
 import java.util.List;
 
 public interface CFLPatientService {
 
     List<Patient> findByVaccinationName(String vaccinationName);
+
+    @Authorized({"Get Patients"})
+    Long getCountOfPatients(String query);
+
+    @Authorized({"Get Patients"})
+    Long getCountOfPatients(String query, boolean includeVoided);
 }
