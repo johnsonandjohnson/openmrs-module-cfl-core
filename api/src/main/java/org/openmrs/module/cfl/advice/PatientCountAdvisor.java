0062e6c065c87714e1f0181f0006ee314589c90f
Piotr Wargulak <pwargulak@soldevelo.com>
2021-12-06 12:23:33 +0100

    AGRE-1843: Code review pt 1
    (cherry picked from commit 85a3573a46ac6650133fe98806a844507626cce4)

--- api/src/main/java/org/openmrs/module/cfl/advice/PatientCountAdvisor.java
+++ api/src/main/java/org/openmrs/module/cfl/advice/PatientCountAdvisor.java
@@ -1,5 +1,6 @@
 package org.openmrs.module.cfl.advice;
 
+import org.openmrs.annotation.OpenmrsProfile;
 import org.openmrs.api.PatientService;
 import org.springframework.aop.Advisor;
 import org.springframework.aop.support.StaticMethodMatcherPointcutAdvisor;
@@ -11,13 +12,14 @@ import java.lang.reflect.Method;
  * org.openmrs.api.PatientService#getCountOfPatients(String)} which applies location based logic to
  * the patient count.
  */
-public class PatientCountAdviser extends StaticMethodMatcherPointcutAdvisor implements Advisor {
+@OpenmrsProfile(modules = "locationbasedaccess:0.2.* - 0.3.*")
+public class PatientCountAdvisor extends StaticMethodMatcherPointcutAdvisor implements Advisor {
   private static final long serialVersionUID = -7809592104506116755L;
 
   private static final Class<?> SERVICE_CLASS = PatientService.class;
   private static final String METHOD_NAME = "getCountOfPatients";
 
-  public PatientCountAdviser() {
+  public PatientCountAdvisor() {
     super(new PatientLocationBasedAccessCountAdvise());
   }
 
--- api/src/main/java/org/openmrs/module/cfl/api/service/CFLPatientService.java
+++ api/src/main/java/org/openmrs/module/cfl/api/service/CFLPatientService.java
@@ -5,13 +5,40 @@ import org.openmrs.annotation.Authorized;
 
 import java.util.List;
 
+/** The Patient service with functions related to the Connect for Life. */
 public interface CFLPatientService {
+  /**
+   * Finds non-voided Patients with assigned {@code vaccinationName} vaccination schedule.
+   *
+   * <p>The vaccination is assigned via Person Attribute with name: {@link
+   * org.openmrs.module.cfl.CFLConstants#VACCINATION_PROGRAM_ATTRIBUTE_NAME}.
+   *
+   * @return the List of Patients with given vaccination, never null
+   */
+  List<Patient> findByVaccinationName(String vaccinationName);
 
-    List<Patient> findByVaccinationName(String vaccinationName);
+  /**
+   * Gets the count of not-voided Patients.
+   *
+   * <p>Compared to {@link org.openmrs.api.PatientService#getCountOfPatients(String)}, this method
+   * applies Location Based Access logic.
+   *
+   * @param query the OpenMRS query, not null
+   * @return the count of not-voided Patients, never null
+   */
+  @Authorized({"Get Patients"})
+  Long getCountOfPatients(String query);
 
-    @Authorized({"Get Patients"})
-    Long getCountOfPatients(String query);
-
-    @Authorized({"Get Patients"})
-    Long getCountOfPatients(String query, boolean includeVoided);
+  /**
+   * Gets the count of Patients, optionally including voided Patients.
+   *
+   * <p>Compared to {@link org.openmrs.api.PatientService#getCountOfPatients(String,boolean)}, this
+   * method applies Location Based Access logic.
+   *
+   * @param query the OpenMRS query, not null
+   * @param includeVoided the flag whether to include voided Patients, or not
+   * @return the count of Patients, never null
+   */
+  @Authorized({"Get Patients"})
+  Long getCountOfPatients(String query, boolean includeVoided);
 }
