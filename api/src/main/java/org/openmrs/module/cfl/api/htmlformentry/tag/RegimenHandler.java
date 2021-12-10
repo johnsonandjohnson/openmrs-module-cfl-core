4fb86cf5d91a261fd77b7687461ad61cfc1aebce
druchniewicz <druchniewicz@soldevelo.com>
2021-12-10 12:57:00 +0100

    Creating regimen obs resources while saving the program form

--- api/src/main/java/org/openmrs/module/cfl/api/htmlformentry/tag/RegimenHandler.java
+++ api/src/main/java/org/openmrs/module/cfl/api/htmlformentry/tag/RegimenHandler.java
@@ -1,21 +1,37 @@
 package org.openmrs.module.cfl.api.htmlformentry.tag;
 
+import org.openmrs.Concept;
+import org.openmrs.module.htmlformentry.FormEntryContext;
 import org.openmrs.module.htmlformentry.FormEntrySession;
 import org.openmrs.module.htmlformentry.FormSubmissionController;
+import org.openmrs.module.htmlformentry.handler.AttributeDescriptor;
 import org.openmrs.module.htmlformentry.handler.SubstitutionTagHandler;
 import org.openmrs.module.htmlformentry.handler.TagHandler;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 import java.util.Map;
 
 public class RegimenHandler extends SubstitutionTagHandler implements TagHandler {
+
+  @Override
+  protected List<AttributeDescriptor> createAttributeDescriptors() {
+    List<AttributeDescriptor> attributeDescriptors = new ArrayList<>();
+    attributeDescriptors.add(new AttributeDescriptor("conceptId", Concept.class));
+    attributeDescriptors.add(new AttributeDescriptor("addNew", String.class));
+    return Collections.unmodifiableList(attributeDescriptors);
+  }
 
   @Override
   protected String getSubstitution(
       FormEntrySession formEntrySession,
       FormSubmissionController formSubmissionController,
       Map<String, String> parameters) {
-    RegimenElement element = new RegimenElement(formEntrySession.getContext());
+    FormEntryContext context = formEntrySession.getContext();
+    RegimenElement element = new RegimenElement(context, parameters);
     formEntrySession.getSubmissionController().addAction(element);
-    return element.generateHtml(formEntrySession.getContext());
+    context.pushToStack(element);
+    return element.generateHtml(context);
   }
 }
