210167ce22811294bf5eb5be4bb2892264687304
jozefowm <mjozefow@soldevelo.com>
2022-01-13 17:05:51 +0100

    WHO-49:Fixing checkstyle errors

--- api/src/main/java/org/openmrs/module/cfl/api/service/impl/IrisPatientServiceImpl.java
+++ api/src/main/java/org/openmrs/module/cfl/api/service/impl/IrisPatientServiceImpl.java
@@ -3,7 +3,6 @@ package org.openmrs.module.cfl.api.service.impl;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.openmrs.Patient;
-import org.openmrs.api.APIException;
 import org.openmrs.api.PatientService;
 import org.openmrs.api.impl.BaseOpenmrsService;
 import org.openmrs.module.cfl.api.service.IrisPatientService;
@@ -13,8 +12,8 @@ import org.springframework.transaction.annotation.Transactional;
 public class IrisPatientServiceImpl extends BaseOpenmrsService implements IrisPatientService {
 
     private PatientService patientService;
-    private static final Log LOGGER = LogFactory.getLog(IrisPatientServiceImpl.class);
 
+    private static final Log LOGGER = LogFactory.getLog(IrisPatientServiceImpl.class);
 
     public void setPatientService(PatientService patientService) {
         this.patientService = patientService;
@@ -25,7 +24,6 @@ public class IrisPatientServiceImpl extends BaseOpenmrsService implements IrisPa
     public Patient savePatient(Patient patient) {
        return patientService.savePatient(patient);
     }
-
 
     public Patient updatePatient(Patient patient) {
 
