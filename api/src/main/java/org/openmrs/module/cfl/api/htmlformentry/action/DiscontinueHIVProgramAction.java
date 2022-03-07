1d6bac3fd269d1fbf9b85823f912c70c66aa8822
druchniewicz <druchniewicz@soldevelo.com>
2022-03-07 16:26:30 +0100

    AGRE-2150: Fetched uuids of HIV program and encounter type from GPs if exist, otherwise default values used
    (cherry picked from commit 64da06f65d2798e7847d9aa622869cbd29adf810)

--- api/src/main/java/org/openmrs/module/cfl/api/htmlformentry/action/DiscontinueHIVProgramAction.java
+++ api/src/main/java/org/openmrs/module/cfl/api/htmlformentry/action/DiscontinueHIVProgramAction.java
@@ -17,10 +17,16 @@ import java.util.Optional;
 
 public class DiscontinueHIVProgramAction implements CustomFormSubmissionAction {
 
-  private static final String HIV_PROGRAM_ENCOUNTER_TYPE_UUID =
+  private static final String HIV_PROGRAM_ENCOUNTER_TYPE_UUID_GP_KEY =
+      "cfl.hivProgramEncounterTypeUuid";
+
+  private static final String HIV_PROGRAM_ENCOUNTER_TYPE_UUID_DEFAULT_VALUE =
       "b3af129b-90c9-4f84-966e-f4f9515083e6";
 
-  private static final String HIV_PROGRAM_UUID = "06c596e7-53dd-44c1-9609-f1406fd9e76d";
+  private static final String HIV_PROGRAM_UUID_GP_KEY = "cfl.hivProgramUuid";
+
+  private static final String HIV_PROGRAM_UUID_DEFAULT_VALUE =
+      "06c596e7-53dd-44c1-9609-f1406fd9e76d";
 
   private static final String VOID_PATIENT_PROGRAM_REASON =
       "Related patient program has been completed";
@@ -42,7 +48,12 @@ public class DiscontinueHIVProgramAction implements CustomFormSubmissionAction {
 
   private Optional<Encounter> getLatestHIVProgramEncounter(Patient patient) {
     EncounterType hivProgramEncounterType =
-        Context.getEncounterService().getEncounterTypeByUuid(HIV_PROGRAM_ENCOUNTER_TYPE_UUID);
+        Context.getEncounterService()
+            .getEncounterTypeByUuid(
+                Context.getAdministrationService()
+                    .getGlobalProperty(
+                        HIV_PROGRAM_ENCOUNTER_TYPE_UUID_GP_KEY,
+                        HIV_PROGRAM_ENCOUNTER_TYPE_UUID_DEFAULT_VALUE));
     List<Encounter> encounters =
         Context.getService(CFLEncounterService.class)
             .getEncountersByPatientAndType(patient, hivProgramEncounterType);
@@ -51,7 +62,10 @@ public class DiscontinueHIVProgramAction implements CustomFormSubmissionAction {
 
   private Optional<PatientProgram> findLatestHIVPatientProgram(Patient patient) {
     ProgramWorkflowService programService = Context.getProgramWorkflowService();
-    Program hivProgram = programService.getProgramByUuid(HIV_PROGRAM_UUID);
+    Program hivProgram =
+        programService.getProgramByUuid(
+            Context.getAdministrationService()
+                .getGlobalProperty(HIV_PROGRAM_UUID_GP_KEY, HIV_PROGRAM_UUID_DEFAULT_VALUE));
     return programService
         .getPatientPrograms(patient, hivProgram, null, null, null, null, false)
         .stream()
